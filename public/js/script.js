6a8b67c295db23250a62a3bbf659abf13397d186
Souban Khandwani <soubankhandwani11@gmail.com>
2025-02-02 17:57:12 +0530

    Project commit

---
+++ public/js/script.js
@@ -0,0 +1,45 @@
+const socket = io();
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit('send-location', { latitude, longitude });
+    },
+    (error) => {
+      console.log(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+} else {
+  console.log('Not working');
+}
+
+const map = L.map('map').setView([0, 0], 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Souban Khandwani',
+}).addTo(map);
+
+const markers = {};
+
+socket.on('receive-location', function (data) {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude]);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
+
+socket.on('user-disconnected', function (id) {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
